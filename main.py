c61bed06f86ae53142efa25ba0d215b67e261249
KzcDuD <72060126+KzcDuD@users.noreply.github.com>
2024-05-13 11:10:30 +0800

    Up

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-from datasets import load_dataset
-
-dataset = load_dataset("jiandong/crimson-attck-vectors",split='train')
-format_func = lambda data: f"id: {data['id']}, attck_id: {data['attck_id']}, 'attck_name{data['attck_name']}', 'description{data['description']}', 'kill_chain_phases{data['kill_chain_phases']}', 'domains{data['domains']}', 'tactic_type{data['tactic_type']}'"
-
-from dotenv import load_dotenv
-import os 
-load_dotenv()
-
-
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_text(format_func(dataset))
-
-# graph
-# embedding 
-vectorstore = FAISS.from_texts(texts=splits, embedding=OllamaEmbeddings())
-vectorstore.save_local("faiss-index")
 new_vector = FAISS.load_local("faiss-index",OllamaEmbeddings(),allow_dangerous_deserialization=True)
 
 retriever = new_vector.as_retriever()
