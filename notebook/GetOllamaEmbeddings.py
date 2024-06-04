ade3a9dd21818a6537c5a540776713b16bd66489
yungchih0209 <chih105015@gmail.com>
2024-06-04 21:55:14 +0800

    mod rag

--- notebook/GetOllamaEmbeddings.py
+++ notebook/GetOllamaEmbeddings.py
@@ -1,7 +1,6 @@
 from datasets import load_dataset
 
-dataset1 = load_dataset("Xcvddax/Attack-tactics",split='train')
-dataset2 = load_dataset("Xcvddax/Attack-mitigations",split='train')
+dataset = load_dataset("Xcvddax/Attack-combine",split='train')
 
 def format_func(datas):
     text = ""
@@ -16,7 +15,7 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_text(format_func(dataset1)+format_func(dataset2))
+splits = text_splitter.split_text(format_func(dataset))
 
 #save vectorstore to local
 vectorstore = FAISS.from_texts(texts=splits, embedding=OllamaEmbeddings())
